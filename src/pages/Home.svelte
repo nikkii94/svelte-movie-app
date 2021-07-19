ab5bb63f5262cd1111a3952f884dfd544d734162
Niki <k.nikolett17@gmail.com>
2021-07-19 19:33:15 +0200

    Initializing components

---
+++ src/pages/Home.svelte
@@ -0,0 +1,33 @@
+<script>
+
+  // Components
+  import Header from '../components/Header.svelte';
+  import Hero from '../components/Hero.svelte';
+  import Search from '../components/Search.svelte';
+  import Grid from '../components/Grid.svelte';
+  import Thumb from '../components/Thumb.svelte';
+  import LoadMoreButton from '../components/LoadMoreButton.svelte';
+  import Spinner from '../components/Spinner.svelte';
+
+  // State
+  let movies = {
+    movies: []
+  };
+  let isLoading;
+  let searchTerm = '';
+  let error;
+
+
+</script>
+
+<Header />
+<Hero />
+<Search />
+<Grid />
+<Thumb />
+<LoadMoreButton />
+<Spinner />
+
+<style>
+
+</style>
