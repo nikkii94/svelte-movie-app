70487aac15045ad820b7ced5fd5c40b88bef3b7e
Niki <k.nikolett17@gmail.com>
2021-07-20 20:40:07 +0200

    Add missing files to git

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,8 @@ import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
-import dotenv from "rollup-plugin-dotenv"
+import replace from '@rollup/plugin-replace';
+import { config } from 'dotenv';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -38,7 +39,14 @@ export default {
 		file: 'public/build/bundle.js'
 	},
 	plugins: [
-		dotenv(),
+		replace({
+			preventAssignment: true,
+			values: {
+				'process.env.API_KEY': production
+					? JSON.stringify(process.env.API_KEY)
+					: JSON.stringify(config().parsed.API_KEY),
+			}
+		}),
 
 		svelte({
 			compilerOptions: {
